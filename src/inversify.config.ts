2f2d37fc14d800ed196bee144e73528ec7d9a556
Anthony Thomas <antwonthegreat@yahoo.com>
2020-08-22 15:11:58 -0600

    initial

---
+++ src/inversify.config.ts
@@ -0,0 +1,19 @@
+import "reflect-metadata";
+import { Container } from "inversify";
+import { TYPES } from "./types";
+import { Bot } from "./bot";
+import { Client } from "discord.js";
+import { PingFinder } from "./services/ping-finder";
+import { MessageResponder } from "./services/message-responder";
+
+let container = new Container();
+
+container.bind<Bot>(TYPES.Bot).to(Bot).inSingletonScope();
+container.bind<Client>(TYPES.Client).toConstantValue(new Client());
+container.bind<string>(TYPES.Token).toConstantValue(process.env.TOKEN);
+container
+  .bind<MessageResponder>(TYPES.MessageResponder)
+  .to(MessageResponder)
+  .inSingletonScope();
+container.bind<PingFinder>(TYPES.PingFinder).to(PingFinder).inSingletonScope();
+export default container;
